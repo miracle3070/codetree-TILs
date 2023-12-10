65537572023f0e01798e73e19cdcd1db1cc632a4
Sangwoo Han <miracle3070@naver.com>
2023-12-10 18:38:28 +0900

    Title: 문자열 한 칸씩 밀어내며 뒤집기, Time: 9ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 231210/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void moveForward(string &str)
+{
+    if(str.length() == 1)
+        return;
+    
+    str = str.substr(1) + str[0];
+}
+
+void moveBackward(string &str)
+{
+    if(str.length() == 1)
+        return;
+
+    int length = str.length();
+    str = str[length-1] + str.substr(0, length-1);
+}
+
+void reverse(string &str)
+{
+    string temp = "";
+    int length = str.length();
+
+    for(int i=length-1; i>=0; i--)
+    {
+        temp += str[i];
+    }
+
+    str = temp;
+}
+
+int main() {
+    int q;
+    string str;
+    cin >> str >> q;
+
+    for(int i=0; i<q; i++)
+    {
+        int command;
+        cin >> command;
+
+        switch(command)
+        {
+        case 1:
+            moveForward(str);
+            break;
+        case 2:
+             moveBackward(str);
+            break;
+        case 3:
+            reverse(str);
+            break;
+        default:
+            break;
+        }
+        cout << str << endl;
+    }
+
+    return 0;
+}
