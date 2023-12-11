970b8208585b38f635b5c67867af7107d590912e
Sangwoo Han <miracle3070@naver.com>
2023-12-11 16:33:36 +0900

    Title: 아스키코드 변환, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231211/아스키코드 변환/convert-to-ascii.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    char ch;
+    int num;
+    cin >> ch >> num;
+
+    cout << (int)ch << " " << (char)num << endl;
+    return 0;
+}
