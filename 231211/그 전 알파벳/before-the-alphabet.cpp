b37f8ac49e55071794c86e9872563ac5bc1d3eaa
Sangwoo Han <miracle3070@naver.com>
2023-12-11 16:46:14 +0900

    Title: 그 전 알파벳, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231211/그 전 알파벳/before-the-alphabet.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    char ch;
+    cin >> ch;
+
+    ch = (char)((ch - 'a' - 1 + 26) % 26 + 'a');
+    cout << ch << endl;
+    return 0;
+}
