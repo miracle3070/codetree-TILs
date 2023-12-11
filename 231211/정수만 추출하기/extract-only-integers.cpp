4f926327ee1fd3069f161503fc94db23d9e83c0c
Sangwoo Han <miracle3070@naver.com>
2023-12-11 17:13:08 +0900

    Title: 정수만 추출하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231211/정수만 추출하기/extract-only-integers.cpp
+++ 231211/정수만 추출하기/extract-only-integers.cpp
@@ -3,28 +3,26 @@
 using namespace std;
 
 int main() {
-    int answer = 0;
     string s1, s2;
+    string sub1="", sub2="";
     cin >> s1 >> s2;
 
     for(int i=0; i<s1.length(); i++)
     {
-        if(!isdigit(s1[i]))
-        {
-            answer += stoi(s1.substr(0, i));
+        if(isdigit(s1[i]))
+            sub1 += s1[i];
+        else
             break;
-        }
     }
 
     for(int i=0; i<s2.length(); i++)
     {
-        if(!isdigit(s2[i]))
-        {
-            answer += stoi(s2.substr(0, i));
+        if(isdigit(s2[i]))
+            sub2 += s2[i];
+        else
             break;
-        }
     }
 
-    cout << answer << endl;
+    cout << stoi(sub1) + stoi(sub2) << endl;
     return 0;
 }
