a0f46104efdff95727d4a29184bbd1dfe7846dce
Sangwoo Han <miracle3070@naver.com>
2023-12-11 16:29:37 +0900

    Title: 아스키코드표 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231211/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int num;
+    
+    for(int i=0; i<5; i++)
+    {
+        cin >> num;
+        cout << (char)num << " ";
+    }
+    
+    return 0;
+}
