d5091545cf79da5a29fb6fae5bc41ab2359f0eb2
Sangwoo Han <miracle3070@naver.com>
2024-01-20 10:47:37 +0900

    Title: 줄 세우기 2, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240120/줄 세우기 2/line-up-students-2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <tuple>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    tuple<int, int, int> students[1000];
+    cin >> n;
+
+    for(int i=0; i<n; i++)
+    {
+        int h, w;
+        cin >> h >> w;
+        students[i] = make_tuple(h, -w, (i+1));
+    }
+
+    sort(students, students+n);
+
+    for(int i=0; i<n; i++)
+    {
+        int h, w, n;
+        tie(h, w, n) = students[i];
+        cout << h << " " << -w << " " << n << endl;
+    }
+
+    return 0;
+}
