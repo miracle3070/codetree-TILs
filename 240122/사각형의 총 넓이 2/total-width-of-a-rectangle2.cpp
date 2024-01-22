38c4d6b9afdb2c13abee1e1141d46d14c067a378
Sangwoo Han <miracle3070@naver.com>
2024-01-22 22:56:36 +0900

    Title: 사각형의 총 넓이 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240122/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int map[201][201] = {0};
+    int total_area = 0;
+    cin >> n;
+
+    for(int i=0; i<n; i++)
+    {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        
+        x1 += 100;
+        y1 += 100;
+
+        x2 += 100;
+        y2 += 100;
+
+        for(int x=x1; x<x2; x++)
+        {
+            for(int y=y1; y<y2; y++)
+            {
+                map[x][y] = 1;
+            }
+        }
+    }
+
+    for(int x=0; x<=200; x++)
+    {
+        for(int y=0; y<=200; y++)
+        {
+            if(map[x][y] == 1)
+            {
+                total_area++;
+            }
+        }
+    }
+
+    cout << total_area << endl;
+    return 0;
+}
